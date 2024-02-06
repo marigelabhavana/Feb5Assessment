ccf90b0d73271ec7771943769a1ca1647fad483e
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-06 17:36:21 +0530

    Add project files.

---
+++ Feb5Assessment/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Feb5Assessment.Models
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public int? JerseyNo { get; set; }
+        public string? Position { get; set; }
+        public string? Team { get; set; }
+    }
+}
